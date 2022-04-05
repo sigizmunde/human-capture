1086b0a2316a9731fa8b2497f70e7897ca290a6f
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-04-05 22:04:11 +0300

    some future features laid by code refactoring

--- script.js
+++ script.js
@@ -1,95 +1,115 @@
-let xmlhttp = new XMLHttpRequest();
-let numberOfImages = -1;
+// let xmlhttp = new XMLHttpRequest();
+// let numberOfImages = -1;
 
-xmlhttp.onreadystatechange = function() {
-if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-    if (xmlhttp.status == 200) {        
-    numberOfImages = (xmlhttp.responseText.match(/title="\d\d.jpg"/g) || []).length;
-    console.log(`There's ${numberOfImages} images`);
-    loadImages();
-    }
-}
-}
+// xmlhttp.onreadystatechange = function() {
+// if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+//     if (xmlhttp.status == 200) {
+//     numberOfImages = (xmlhttp.responseText.match(/title="\d\d.jpg"/g) || []).length;
+//     console.log(`There's ${numberOfImages} images`);
+//     loadImages();
+//     }
+// }
+// }
 
-xmlhttp.open("GET", './images', true);
-xmlhttp.send();
+// xmlhttp.open("GET", './images', true);
+// xmlhttp.send();
+
+const textContent = {
+  en: {
+    heading:
+      'Select all images with <span class="capcha__heading--bold">humans</span>',
+    subHeading: 'Click "Verify" once there are none left',
+    alertPositive:
+      "You're right, no russian troops or officials seem to be human beings",
+    alertNegative:
+      "Let's face the truth: there are no humans among russians.\nTry again.",
+    alertDefault: "Seems, you have not checked any images",
+    btnCaption: "Verify",
+  },
+  ua: {
+    subHeading: "Натисніть «Перевірити», відмітивши всі відповідні фото",
+    heading:
+      'Позначте всі фото, на фких зображено<span class="capcha__heading--bold">людей</span>',
+    alertPositive: "Хіба русня не люди? Да ето так.\nВітаємо, ви пройшли тест!",
+    alertNegative: "Ну що ж ви, хіба русня — то люди?\nДавайте ще раз.",
+    alertDefault: "Схоже, ви не обрали жодної світлини",
+    btnCaption: "Перевірити",
+  },
+};
+
+const alerts = assignAlerts("en");
 
 //-------------------------------------------------------------------
-numberOfImages = 15;
-loadImages();
+const numberOfImages = 15;
+window.addEventListener("load", loadImages);
 //-------------------------------------------------------------------
 
-const submitBtn = document.querySelector('.capcha__submit-btn');
+const submitBtn = document.querySelector(".capcha__submit-btn");
+submitBtn.addEventListener("click", onSubmitBtnClick);
+
+const langSwitch = document.querySelector(".capcha__icons");
+langSwitch.addEventListener("click", onLangSwitchClick);
 
 function loadImages() {
-    const numArr = randomIntegerArray(9, numberOfImages);
-    const itemArr = numArr.map(createItemEl);
-    for (const item of itemArr) {
-        item.addEventListener('click', onItemClick);
-    }
-    const containerGrid = document.querySelector('.capcha__grid');
-    containerGrid.innerHTML = '';
-    containerGrid.append(...itemArr);
+  const numArr = randomIntegerArray(9, numberOfImages);
+  const itemArr = numArr.map(createItemEl);
+  for (const item of itemArr) {
+    item.addEventListener("click", onItemClick);
+  }
+  const containerGrid = document.querySelector(".capcha__grid");
+  containerGrid.innerHTML = "";
+  containerGrid.append(...itemArr);
 }
 
 function createItemEl(number) {
-    const newEl = document.createElement('li');
-    const fileName = (number + '').padStart(2,0);
-    newEl.innerHTML = (`<img src="./images/${fileName}.jpg" alt="capture image" class="capcha__img" />`);
-    newEl.classList.add('capcha__item');
-    console.log(newEl);
-    return newEl;
-  }
-
-function onItemClick(event) {
-    event.currentTarget.classList.toggle('capcha__item--marked');
+  const newEl = document.createElement("li");
+  const fileName = (number + "").padStart(2, 0);
+  newEl.innerHTML = `<img src="./images/${fileName}.jpg" alt="capture image" class="capcha__img" />`;
+  newEl.classList.add("capcha__item");
+  // console.log(newEl);
+  return newEl;
 }
 
-submitBtn.addEventListener('click', onSubmitBtnClick);
+function onItemClick(event) {
+  event.currentTarget.classList.toggle("capcha__item--marked");
+}
 
 function onSubmitBtnClick() {
-    alert('There is no humans among russians.\nTry again.');
-    location.reload();
+  if (document.querySelector(".capcha__icons--marked").length > 0) {
+    alert(alerts.negative);
+  }
+  location.reload();
 }
 
 function randomIntegerArray(targetLength, range) {
-    const cycleTreshold = 2000;
-    const intArr = [];
-    let n = 0;
-    while ((intArr.length < targetLength) && (n < cycleTreshold))
-    {
-        const newInt = Math.ceil(Math.random() * range);
-        if (intArr.indexOf(newInt) === -1) {
-            intArr.push(newInt);
-        }
-        n += 1;
+  const cycleTreshold = 2000;
+  const intArr = [];
+  let n = 0;
+  while (intArr.length < targetLength && n < cycleTreshold) {
+    const newInt = Math.ceil(Math.random() * range);
+    if (intArr.indexOf(newInt) === -1) {
+      intArr.push(newInt);
     }
-    console.log(`Generated intArr ${intArr}`)
-    return intArr;
+    n += 1;
+  }
+  console.log(`Generated intArr ${intArr} on ${n} iteration`);
+  return intArr;
 }
 
-// function getNumberOfJpegs(folder) {
+function onLangSwitchClick() {
+  switchLang();
+}
 
-//     let xmlhttp = new XMLHttpRequest();
-//     let numberOfImages = -1;
+function switchLang(lang) {
+  textContent[lang];
+}
 
-//     xmlhttp.onreadystatechange = function() {
-//     if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-//         if (xmlhttp.status == 200) {        
-//         return (xmlhttp.responseText.match(/title="\d\d.jpg"/g) || []).length;
-//         //   for (let i = 1; i <= numberOfImages; i++) {
-//         //     let img1 = document.createElement('img');
-//         //     img1.src = './images/' + i.toString().padStart(2,'0') + '.jpg';
-//         //     document.body.appendChild(img1);
-//         //   }
-//         }
-//     }
-//     }
-
-//     xmlhttp.open("GET", folder, true);
-//     xmlhttp.send();
-
-//     console.log(`There's ${numberOfImages} images`)
-//     return numberOfImages;
-    
-// }
+function assignAlerts(lang) {
+  const { alertPositive, alertNegative, alertDefault, ...rest } =
+    textContent[lang];
+  return {
+    positive: alertPositive,
+    negative: alertNegative,
+    default: alertDefault,
+  };
+}
